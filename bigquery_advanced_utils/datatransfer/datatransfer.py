1a5a85aa8c7b59743c4a80f8f27c13b6e426c131
Alessio-Siciliano <alessiosiciliano@outlook.it>
2024-12-14 23:04:59 +0100

    Improve the class by removing many parameters.

--- bigquery_advanced_utils/datatransfer/datatransfer.py
+++ bigquery_advanced_utils/datatransfer/datatransfer.py
@@ -4,17 +4,17 @@ import re
 from typing import Optional, Sequence, Tuple, Union
 from google.cloud.bigquery_datatransfer import DataTransferServiceClient
 from google.auth.credentials import Credentials
-from google.cloud.bigquery_datatransfer_v1.services.data_transfer_service.pagers import (  # pylint: disable=line-too-long
-    ListTransferConfigsPager,
-)
 from google.cloud.bigquery_datatransfer_v1.types.datatransfer import (
     ListTransferConfigsRequest,
 )
 from google.api_core.retry import Retry
 from google.api_core.gapic_v1.method import _MethodDefault
-from google.cloud.bigquery_datatransfer_v1.types.transfer import TransferConfig
 
 from bigquery_advanced_utils.utils.string import String
+from bigquery_advanced_utils.bigquery.bigquery import BigQueryClient
+from bigquery_advanced_utils.datatransfer.extended_transfer_config import (
+    ExtendedTransferConfig,
+)
 
 
 class DataTransferClient(DataTransferServiceClient):
@@ -33,15 +33,20 @@ class DataTransferClient(DataTransferServiceClient):
 
     def __init__(
         self,
-        credentials: Optional[Credentials] = None,
+        credentials: Credentials,
+        project_id: str,
+        location: str = "US",
         client_options: Optional[dict] = None,
     ) -> None:
         """Init of the class, same as parent
 
         Parameters
         ----------
-        credentials : Optional[Credentials]
+        credentials : Credentials
             credentials of the current user
+
+        location: str
+            Location of the data. Default: US
 
         client_options: Optional[dict]
             custom client settings
@@ -53,30 +58,30 @@ class DataTransferClient(DataTransferServiceClient):
         super().__init__(
             credentials=credentials, client_options=client_options
         )
-        self.cached_iterator: dict = {}
+        self.location = location
+        self.project_id = project_id
         self.string_utils = String()
+        self.bigquery_client = BigQueryClient()
+        self.cached_transfer_configs_list: list[ExtendedTransferConfig] = []
 
-    def list_transfer_configs(
+        self.parent = f"projects/{self.project_id}/locations/{self.location}"
+
+    def get_transfer_configs(
         self,
         request: Optional[Union[ListTransferConfigsRequest, dict]] = None,
         *,
-        parent: Optional[str] = None,
+        # parent: Optional[str] = None,
         retry: Optional[Union[Retry, _MethodDefault, None]] = None,
         timeout: Optional[Union[float, object]] = None,
         metadata: Sequence[Tuple[str, str]] = (),
-        with_email: bool = False,
-    ) -> ListTransferConfigsPager:
+        additional_configs: bool = False,
+    ) -> list["ExtendedTransferConfig"]:
         """Get ALL schedule queries of the project.
 
         Parameters
         ----------
         request:
             A request to list data transfers configured for a BigQuery project.
-
-        parent:
-            The BigQuery project id, it should be returned:
-                projects/{project_id}/locations/{location_id} or
-                projects/{project_id}
 
         retry:
             Designation of what errors, if any, should be retried.
@@ -87,14 +92,14 @@ class DataTransferClient(DataTransferServiceClient):
         metadata: Sequence[Tuple[str, str]]
             Sequence of metadata as the original function.
 
-        with_email: bool
-            this field makes another request to get the owner_email.
+        additional_configs: bool
+            this field makes another request to get more informations.
             Default value is False to avoid useless requests.
 
         Returns
         -------
-        ListTransferConfigsPager
-            Iterator of the TransConfigPager
+        List[ExtendedTransferConfig]
+            Iterator of the ExtendedTransferConfig
 
         Raises
         -------
@@ -107,12 +112,13 @@ class DataTransferClient(DataTransferServiceClient):
             request = ListTransferConfigsRequest(**request)
 
         # At least one between request and parent should be not empty
-        if (request is None or not request.parent) and not parent:
+        if (request is None or not request.parent) and not self.parent:
             raise ValueError("Request or parent parameters must be provided!")
 
         if (
-            parent is not None
-            and re.match(self.MATCHING_RULE_PROJECT_LOCATION, parent) is None
+            self.parent is not None
+            and re.match(self.MATCHING_RULE_PROJECT_LOCATION, self.parent)
+            is None
         ):
             raise ValueError(
                 "Parent should be in the format projects/{}/locations/{}"
@@ -120,26 +126,43 @@ class DataTransferClient(DataTransferServiceClient):
 
         transfer_configs_request_response = super().list_transfer_configs(
             request=request,
-            parent=parent,
+            parent=self.parent,
             retry=retry,
             timeout=timeout,
             metadata=metadata,
         )
-        if with_email:
-            # For each TransferConfig we make a single request to get the email
-            for transfer_config in transfer_configs_request_response:
-                # If the data format is correct, let's try with the request
+        # Clears all elements from the list
+        self.cached_transfer_configs_list.clear()
+
+        # For each TransferConfig we make a single request to get the email
+        for transfer_config_original in transfer_configs_request_response:
+            transfer_config = ExtendedTransferConfig(transfer_config_original)
+            if additional_configs:
                 transfer_config_email = self.get_transfer_config(
-                    name=transfer_config.name
+                    name=transfer_config.base_config.name
                 ).owner_info
-                setattr(transfer_config, "owner_info", transfer_config_email)
+                transfer_config.additional_configs["owner_email"] = (
+                    transfer_config_email
+                ).email
 
-        self.cached_iterator[with_email] = transfer_configs_request_response
-        return transfer_configs_request_response
+                # Care this can trigger an exception if fails
+                simulated_attributes = self.bigquery_client.simulate_query(
+                    transfer_config.base_config.params.get("query")
+                )
 
-    def list_transfer_config_by_owner_email(
-        self, owner_email: str, parent: str
-    ) -> list[TransferConfig]:
+                transfer_config.additional_configs[
+                    "total_estimated_processed_bytes"
+                ] = simulated_attributes.get("total_bytes_processed")
+                transfer_config.additional_configs["referenced_tables"] = (
+                    simulated_attributes.get("referenced_tables")
+                )
+            self.cached_transfer_configs_list.append(transfer_config)
+            break
+        return self.cached_transfer_configs_list
+
+    def get_transfer_configs_by_owner_email(
+        self, owner_email: str
+    ) -> list[ExtendedTransferConfig]:
         """Get ALL schedule queries of a given user.
 
         Parameters
@@ -147,15 +170,10 @@ class DataTransferClient(DataTransferServiceClient):
         owner_email:
             Owner of the scheduled query.
 
-        parent:
-            The BigQuery project id, it should be in the format:
-                projects/{project_id}/locations/{location_id} or
-                projects/{project_id}
-
         Returns
         -------
-        list[TransferConfig]
-            List of all TransferConfig object
+        list[ExtendedTransferConfig]
+            List of all ExtendedTransferConfig object
 
         Raises
         -------
@@ -165,21 +183,25 @@ class DataTransferClient(DataTransferServiceClient):
         """
 
         # If not cached, run it
-        if True not in self.cached_iterator:
-            self.cached_iterator[True] = self.list_transfer_configs(
-                parent=parent, with_email=True
+        if (
+            len(self.cached_transfer_configs_list) == 0
+            or self.cached_transfer_configs_list[0].additional_configs == {}
+        ):
+            self.cached_transfer_configs_list = self.get_transfer_configs(
+                additional_configs=True
             )
 
         return list(
             filter(
-                lambda x: x.owner_info.email == owner_email,
-                self.cached_iterator[True],
+                lambda x: x.additional_configs.get("owner_email")
+                == owner_email.lower(),
+                self.cached_transfer_configs_list,
             )
         )
 
-    def list_transfer_configs_by_table(
-        self, table_id: str, parent: str
-    ) -> list[TransferConfig]:
+    def get_transfer_configs_by_table_id(
+        self, table_id: str
+    ) -> list[ExtendedTransferConfig]:
         """List transfer configs by table in the query
 
         Parameters
@@ -187,37 +209,29 @@ class DataTransferClient(DataTransferServiceClient):
         table_id:
             Name of the table (not needed entire path).
 
-        parent:
-            The BigQuery project id, it should be in the format:
-                projects/{project_id}/locations/{location_id} or
-                projects/{project_id}
-
         Returns
         -------
-        list[TransferConfig]
+        list[ExtendedTransferConfig]
             List of all TransferConfig object
 
         """
-
         # If not cached, run it
-        if len(self.cached_iterator.keys()) == 0:
-            self.cached_iterator[False] = self.list_transfer_configs(
-                parent=parent
+        if (
+            len(self.cached_transfer_configs_list) == 0
+            or self.cached_transfer_configs_list[0].additional_configs == {}
+        ):
+            self.cached_transfer_configs_list = self.get_transfer_configs(
+                additional_configs=True
             )
-
         return list(
             filter(
                 lambda x: table_id.lower()
                 in [
                     t.lower().split(".")[-1]
                     for t in self.string_utils.extract_tables_from_query(
-                        x.params.get("query")
+                        x.base_config.params.get("query")
                     )
                 ],
-                (
-                    self.cached_iterator[False]
-                    if False in self.cached_iterator
-                    else self.cached_iterator[True]
-                ),
+                self.cached_transfer_configs_list,
             )
         )
